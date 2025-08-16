67eee787068a407d3024a5d75b5f4a858fc4fcb7
Saptyadeep <bsaptyadeep@gmail.com>
2025-08-16 16:15:04 +0530

    updated the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>My app</title>
+        <title>zustbio</title>
       </head>
       <body suppressHydrationWarning={true}>
         {children}
